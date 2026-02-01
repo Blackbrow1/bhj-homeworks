d9fd3191ef6fc50c2138399c80c99769be276369
Александр Чернобров <alesandro83@mail.ru>
2026-02-01 20:50:21 +0300

    fix: Исправил код dropdown

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,18 +1,22 @@
-const dropdownValue = document.querySelector('.dropdown__value');
-const dropdownList = document.querySelector('.dropdown__list');
-const dropdownItems = Array.from(document.querySelectorAll('.dropdown__item'));
+const dropdowns = Array.from(document.querySelectorAll('.dropdown'));
 
-dropdownValue.addEventListener('click', () => {
-  dropdownList.classList.toggle('dropdown__list_active');
-});
+dropdowns.forEach(dropdown => {
+  const dropdownValue = dropdown.querySelector('.dropdown__value');
+  const dropdownList = dropdown.querySelector('.dropdown__list');
+  const dropdownItems = Array.from(dropdown.querySelectorAll('.dropdown__item'));
 
-dropdownItems.forEach(item => {
-  const link = item.querySelector('.dropdown__link');
-  
-  link.addEventListener('click', (event) => {
-    event.preventDefault();
+  dropdownValue.addEventListener('click', () => {
+    dropdownList.classList.toggle('dropdown__list_active');
+  });
+
+  dropdownItems.forEach(item => {
+    const link = item.querySelector('.dropdown__link');
     
-    dropdownValue.textContent = link.textContent;
-    dropdownList.classList.remove('dropdown__list_active');
+    link.addEventListener('click', (event) => {
+      event.preventDefault();
+      
+      dropdownValue.textContent = link.textContent;
+      dropdownList.classList.remove('dropdown__list_active');
+    });
   });
 });
