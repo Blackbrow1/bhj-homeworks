c3c677d48842798daa87565470b658ce9c6e14b3
Александр Чернобров <alesandro83@mail.ru>
2026-02-01 21:11:02 +0300

    fix: исправил код tabs

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,20 +1,24 @@
-const tab = Array.from(document.querySelectorAll('.tab'));
-const tabContent = Array.from(document.querySelectorAll('.tab__content'));
+const tabs = Array.from(document.querySelectorAll('.tabs'));
 
-tab.forEach((item, idx) => {
-  item.addEventListener('click', () => {
-    tab.forEach(itm => {
-      itm.classList.remove('tab_active');
-    });
+tabs.forEach(tabBlock => {
+  const tab = Array.from(tabBlock.querySelectorAll('.tab'));
+  const tabContent = Array.from(tabBlock.querySelectorAll('.tab__content'));
 
-    tab[idx].classList.add('tab_active');
+  tab.forEach((item, idx) => {
+    item.addEventListener('click', () => {
+      tab.forEach(itm => {
+        itm.classList.remove('tab_active');
+      });
 
-    tabContent.forEach((item, index) => {
-      item.classList.remove('tab__content_active');
+      tab[idx].classList.add('tab_active');
 
-      if (index === idx) {
-        item.classList.add('tab__content_active');
-      }
+      tabContent.forEach((item, index) => {
+        item.classList.remove('tab__content_active');
+
+        if (index === idx) {
+          item.classList.add('tab__content_active');
+        }
+      });
     });
   });
 });
